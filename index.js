86f038d97dc25c13c04e20a4729dcc54cda3be61
gabriel <jesusdesilva78@gmail.com>
2023-05-27 16:09:41 -0300

    Components of page

--- index.js
+++ index.js
@@ -1,43 +1,43 @@
 
-
-
-
-
 class Mp3 {
 
     constructor() {
         this.audioMp3 = document.querySelector('#soundMp3');
         this.arraySounds = [
-          
             {
                 song: 'src/sounds/INTERWORLD _METAMORPHOSIS__317RHaFF7Xk.mp3',
                 nameSong: 'INTERWORLD "METAMORPHOSIS"',
                 img: 'https://i1.sndcdn.com/artworks-oJ0eLUhtByvCe00k-4BM8Mg-t500x500.jpg'
 
             },
-      ];
 
-        this.checker = 0;
+        ];
+
+        this.checkerButtonPlay = 0;
         this.indexSong = 0
 
         //evento de escuta no audio para dar uma atualização no tempo da musica  
         this.toListen = document.querySelector('audio').addEventListener('timeupdate', this.renderingRage_timeSong)
 
     }
-
+    indexSongScreen() {
+        this.audioMp3.src = this.arraySounds[0].song;
+        document.querySelector('#nameSongs').innerHTML = this.arraySounds[0].nameSong;
+        document.querySelector('.telaImg').src = this.arraySounds[0].img;
+    }
     play() {
         this.audioMp3.play();
-        if (this.checker++ >= 1) {
+        if (this.checkerButtonPlay++ >= 1) {
             this.pause(this.audioMp3);
-            document.querySelector('.playBt').classList.remove('pauseBt')
+            document.querySelector('.play').classList.remove('pause')
         } else {
-            document.querySelector('.playBt').classList.add('pauseBt')
+            document.querySelector('.play').classList.add('pause')
         }
 
     }
 
     pause(audioMp3) {
-        return audioMp3.pause(), this.checker = 0
+        return audioMp3.pause(), this.checkerButtonPlay = 0
 
     }
 
@@ -45,60 +45,64 @@ class Mp3 {
 
         this.audioMp3.src = this.arraySounds[index].song;
         document.querySelector('#nameSongs').innerHTML = this.arraySounds[index].nameSong;
-        document.querySelector('img').src = this.arraySounds[index].img;
+        document.querySelector('.telaImg').src = this.arraySounds[index].img;
 
         this.audioMp3.play();
 
     }
-    loop(){
+    renderLIstSongs() {
         for (let i = 0; i < this.arraySounds.length; i++) {
-        const img = document.createElement('img');
-        img.classList.add('cardImg')
-        const card = document.createElement('div');
-        const text = document.createElement('p')
-        const container = document.querySelector('.containerListSongs')
-        img.src = this.arraySounds[i].img
-        text.innerHTML = this.arraySounds[i].nameSong
-        card.classList.add('card');
-        card.appendChild(img)
-        card.appendChild(text)
-        container.appendChild(card)
-        }
-    }
-
-    listSongs() {
-        const img = document.createElement('img');
-        img.classList.add('cardImg')
-        const card = document.createElement('div');
-        const container = document.querySelector('.containerListSongs')
-        const text = document.createElement('p')
-        for (let i = 0; i < this.arraySounds.length; i++) {
-            text.innerHTML = this.arraySounds[i].nameSong
+            const img = document.createElement('img');
+            img.classList.add('cardImg')
+            const card = document.createElement('div');
+            const text = document.createElement('p')
+            const container = document.querySelector('.containerListSongs')
             img.src = this.arraySounds[i].img
+            text.innerHTML = this.arraySounds[i].nameSong
             card.classList.add('card');
             card.appendChild(img)
             card.appendChild(text)
             container.appendChild(card)
+        }
+    }
+
+    CreateListSongs() {
+        const container = document.querySelector('.containerListSongs')
+        const img = document.createElement('img');
+        const card = document.createElement('div');
+        const text = document.createElement('p');
+
+        img.classList.add('cardImg');
+        card.classList.add('card');
+
+        for (let i = 0; i < this.arraySounds.length; i++) {
+
+            text.innerHTML = this.arraySounds[i].nameSong.toLowerCase();
+            img.src = this.arraySounds[i].img;
+
+
+            card.appendChild(img);
+            card.appendChild(text);
+            container.appendChild(card);
         }
 
     }
 
     addSongs() {
         const inputFiles = document.querySelector('#filesSongs');
-        const files = inputFiles.files[0]
-        const { name, size } = files
-        const imgUrl = URL.createObjectURL(files)
-      console.log(files)
-        this.arraySounds.push({  song: imgUrl,nameSong:name.slice(0,18),img:"https://media.tenor.com/zNQYPkNYOAoAAAAM/spongebob-dance.gif" })
- 
-        // console.log(this.arraySounds)
- this.listSongs()
+        const files = inputFiles.files[0];
+        const { name, size } = files;
+        const imgUrl = URL.createObjectURL(files);
+
+        this.arraySounds.push({ song: imgUrl, nameSong: name.slice(0, 22), img: "https://i.pinimg.com/originals/07/a4/5e/07a45e02da91562edcc341664e85fdf8.gif" })
+
+        this.CreateListSongs()
 
     }
     renderingRage_timeSong() {
-
         //range da musica
         var audioMp3 = document.querySelector('#soundMp3');
+
         let range = document.querySelector('#range');
         range.max = audioMp3.duration
         range.value = audioMp3.currentTime;
@@ -109,21 +113,21 @@ class Mp3 {
         });
 
         //ttempo decorrido da musica
-        let timeCurrent = document.querySelector('#currentTimer');
+        let timeCurrent = document.querySelector('#currentTime');
         let seconds = Math.floor(audioMp3.currentTime);
         let timeMinutes = Math.floor(seconds / 60);
         let timeSeconds = seconds % 60;
 
         if (timeSeconds < 10) timeSeconds = '0' + timeSeconds;
-        timeCurrent.innerHTML = timeMinutes + ':' + timeSeconds;
+        timeCurrent.innerHTML = "0" + timeMinutes + ':' + timeSeconds;
 
         //duração da musica
         let timeSong = document.querySelector('#timeSong');
         let durationSongMinutes = Math.floor(audioMp3.duration / 60);
         let durationSongSeconds = Math.floor(audioMp3.duration % 60);
 
-        console.log(audioMp3.duration)
-if(durationSongSeconds<10) durationSongSeconds = "0"+durationSongSeconds
+
+        if (durationSongSeconds < 10) durationSongSeconds = "0" + durationSongSeconds
         //load da duração da musica quando a musica for alternada 
         const checkerTimeSong = timeSong.innerHTML = `0${durationSongMinutes}:${durationSongSeconds}`
         if (checkerTimeSong != '0NaN:NaN') timeSong.classList.remove('imgLoad')
@@ -133,26 +137,31 @@ if(durationSongSeconds<10) durationSongSeconds = "0"+durationSongSeconds
 
     }
 
+
+
     // a vança para proxima musica
     next() {
-        document.querySelector('.playBt').classList.add('pauseBt');
-
+        document.querySelector('.play').classList.add('pause');
         this.indexSong++
+
         //verifica se a quantidade de clicks foi igual ao total de musicas na array
         if (this.indexSong == this.arraySounds.length) this.indexSong = 0;
         this.renderingSongs(this.indexSong);
-        this.audioMp3.play()
+
     }
 
     //volta na musica anterior
     previus() {
-        document.querySelector('.playBt').classList.add('pauseBt');
+        document.querySelector('.play').classList.add('pause');
 
         this.indexSong--
-        //verifica se a quantidade de clicks foi igual ao total de musicas na array
-        if (this.indexSong < 0) this.indexSong = 5;
+        if (this.indexSong == -1) this.indexSong = this.arraySounds.length
         this.renderingSongs(this.indexSong);
-        this.audioMp3.play()
+
+    }
+
+    music_speed() {
+        this.audioMp3.playbackRate = 1.5
     }
 }
 
@@ -160,4 +169,5 @@ if(durationSongSeconds<10) durationSongSeconds = "0"+durationSongSeconds
 var mp3 = new Mp3()
 
 
-mp3.loop()
+mp3.indexSongScreen()
+mp3.renderLIstSongs()
