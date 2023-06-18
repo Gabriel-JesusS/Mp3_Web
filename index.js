5a1d7b2e9b0b13dc6fdbcce33c9b17a339f46cd3
gabriel <jesusdesilva78@gmail.com>
2023-06-18 17:42:51 -0300

    js code added new features

--- index.js
+++ index.js
@@ -1,46 +1,37 @@
-
+//ass: Mr Sif
 class Mp3 {
-
+    static cont = 0
     constructor() {
         this.audioMp3 = document.querySelector('#soundMp3');
         this.arraySounds = [];
         this.checkerButtonPlay = 0;
         this.indexSong = 0;
+        this.checkerFirtsSongAdd = true
         //evento de escuta no audio para dar uma atualização no tempo da musica  
         this.toListen = document.querySelector('audio').addEventListener('timeupdate', this.renderingRage_timeSong)
 
-    }
-
-
-    indexSongScreen() {
-        this.audioMp3.src = this.arraySounds[0].song;
-        document.querySelector('#nameSongs').innerHTML = this.arraySounds[0].nameSong;
-        document.querySelector('.telaImg').src = this.arraySounds[0].img;
     }
 
     play() {
 
         if (this.audioMp3.readyState >= 4) { // Verifica se o áudio está pronto para ser reproduzido
             this.audioMp3.play();
-            if (this.checkerButtonPlay++ >= 1) {
-                this.pause();
-                document.querySelector('.play').classList.remove('pause');
-            } else {
-                document.querySelector('.play').classList.add('pause');
-            }
+            this.checkerButtonPlay++ >= 1 ? this.pause() : document.querySelector('.play').classList.add('pause');
+
         }
     }
 
     pause() {
         this.audioMp3.pause();
         this.checkerButtonPlay = 0;
+        document.querySelector('.play').classList.remove('pause');
     }
 
 
     renderingSongs(index) {
         this.audioMp3.src = this.arraySounds[index].song;
         document.querySelector('#nameSongs').innerHTML = this.arraySounds[index].nameSong;
-        document.querySelector('.telaImg').src = this.arraySounds[index].img;
+        // document.querySelector('.telaImg').src = this.arraySounds[index].img;
         document.querySelector('.play').classList.add('pause');
         this.audioMp3.play();
         this.checkerButtonPlay = 1;
@@ -55,16 +46,18 @@ class Mp3 {
     //METODO PARA CRIAR LISTA DAS MUSICAS ADICIONADAS
     CreateListSongs() {
         const container = document.querySelector('.containerListSongs');
-        const cards = document.querySelectorAll('.card')
         const img = document.createElement('img');
         const card = document.createElement('div');
         const text = document.createElement('p');
         const deleteSong = document.createElement('button');
 
+
         img.classList.add('cardImg');
         card.classList.add('card');
         deleteSong.innerHTML = 'Delete';
-        deleteSong.classList.add('delete')
+        deleteSong.classList.add('delete');
+
+        this.totalSongs(Mp3.cont += 1)
 
         for (let i = 0; i < this.arraySounds.length; i++) {
 
@@ -73,19 +66,18 @@ class Mp3 {
             card.appendChild(img);
             card.appendChild(text);
             card.appendChild(deleteSong);
-
             //RENDERIZA A MUSICA DA LISTA QUE FOI CLICADA FAZENDO O CARD RECEBER UM EVENTO DE ONCLICK
             card.setAttribute('onclick', `mp3.renderingSongs(${i})`);
 
 
             deleteSong.addEventListener('click', () => {
+                this.totalSongs(Mp3.cont - 1)
                 document.querySelector('.play').classList.remove('pause');
                 this.arraySounds.splice(i, 1);
                 card.remove();
                 this.pause();
                 console.log(this.arraySounds)
             });
-
 
             container.appendChild(card);
         }
@@ -97,10 +89,13 @@ class Mp3 {
         return string.charAt(0).toUpperCase() + string.slice(1, 25).toLowerCase() + "..."
     }
 
+    totalSongs(cont) {
+
+        document.querySelector("#mySongs").innerHTML = cont
+    }
+
     //METODO PARA CAPTURAR OS ARQUIVOS ADICIONADOS
     addSongs() {
-        let checkerFirtsSongAdd = true
-
         const inputFiles = document.querySelector('#filesSongs');
         const files = inputFiles.files[0];
         //DESTRUCTION DO OBJETO ARQUIVO PARA SER ADIONADO ARRAY DE OBJETOS "ARRAYSOUNDS"
@@ -109,83 +104,83 @@ class Mp3 {
         const imgUrl = URL.createObjectURL(files);
 
         this.arraySounds.push({ song: imgUrl, nameSong: this.formatStrings(name), img: "https://dk2dv4ezy246u.cloudfront.net/widgets/sSUHepLQIwU_large.jpg" })
-
-        if (checkerFirtsSongAdd == true) {
+        this.checkerFirtsSongAdd
+        if (this.checkerFirtsSongAdd == true) {
             this.renderingSongs(0);
+            this.checkerFirtsSongAdd = false
         }
         this.CreateListSongs();
     }
 
-        renderingRage_timeSong() {
-            let audioMp3 = document.querySelector('#soundMp3');
+    renderingRage_timeSong() {
+        let audioMp3 = document.querySelector('#soundMp3');
 
-            //range da musica
-            let range = document.querySelector('#range');
+        //range da musica
+        let range = document.querySelector('#range');
 
-            //RANGE.MAX DEFINE O LIMITE DO RANGE, RANGE.VALUE CONRRESPONDE AO SEU VALOR DE POSIÇÃO ELE ESTÁ RECEBENDO O TEMPO CORRIDO DA MUSÍCA 
-            range.max = audioMp3.duration;
-            range.value = audioMp3.currentTime;
+        //RANGE.MAX DEFINE O LIMITE DO RANGE, RANGE.VALUE CONRRESPONDE AO SEU VALOR DE POSIÇÃO ELE ESTÁ RECEBENDO O TEMPO CORRIDO DA MUSÍCA 
+        range.max = audioMp3.duration;
+        range.value = audioMp3.currentTime;
 
-            // ADIOCIONAMOS UM EVENTO DE ESUCUTA AO RANGE PARA QUE QUANDO O USUARIO DESLIZAR O RANGE O TEMPO DA MUSICA MUDARA  
-            range.addEventListener('input', () => {
-                audioMp3.currentTime = range.value * range.max / 100;
-                audioMp3.play()
-            });
+        // ADIOCIONAMOS UM EVENTO DE ESUCUTA AO RANGE PARA QUE QUANDO O USUARIO DESLIZAR O RANGE O TEMPO DA MUSICA MUDARA  
+        range.addEventListener('input', () => {
+            audioMp3.currentTime = range.value * range.max / 100;
+            audioMp3.play()
+        });
 
-            //tempo decorrido da musica
-            let timeCurrent = document.querySelector('#currentTime');
-            let seconds = Math.floor(audioMp3.currentTime);
-            let timeMinutes = Math.floor(seconds / 60);
-            let timeSeconds = seconds % 60;
+        //tempo decorrido da musica
+        let timeCurrent = document.querySelector('#currentTime');
+        let seconds = Math.floor(audioMp3.currentTime);
+        let timeMinutes = Math.floor(seconds / 60);
+        let timeSeconds = seconds % 60;
 
-            if (timeSeconds < 10) timeSeconds = '0' + timeSeconds;
-            timeCurrent.innerHTML = "0" + timeMinutes + ':' + timeSeconds;
+        if (timeSeconds < 10) timeSeconds = '0' + timeSeconds;
+        timeCurrent.innerHTML = "0" + timeMinutes + ':' + timeSeconds;
 
-            //duração da musica
-            let timeSong = document.querySelector('#timeSong');
-            let durationSongMinutes = Math.floor(audioMp3.duration / 60);
-            let durationSongSeconds = Math.floor(audioMp3.duration % 60);
-            if (durationSongSeconds < 10) durationSongSeconds = "0" + durationSongSeconds
-
-
-            const checkerTimeSong = timeSong.innerHTML = `0${durationSongMinutes}:${durationSongSeconds}`
-            //load da duração da musica quando a musica for alternada 
-            if (checkerTimeSong != '0NaN:NaN') timeSong.classList.remove('imgLoad')
-            if (checkerTimeSong == '0NaN:NaN') timeSong.classList.add('imgLoad'), timeSong.innerHTML = ''
+        //duração da musica
+        let timeSong = document.querySelector('#timeSong');
+        let durationSongMinutes = Math.floor(audioMp3.duration / 60);
+        let durationSongSeconds = Math.floor(audioMp3.duration % 60);
+        if (durationSongSeconds < 10) durationSongSeconds = "0" + durationSongSeconds
 
 
+        const checkerTimeSong = timeSong.innerHTML = `0${durationSongMinutes}:${durationSongSeconds}`
+        //load da duração da musica quando a musica for alternada 
+        if (checkerTimeSong != '0NaN:NaN') timeSong.classList.remove('imgLoad')
+        if (checkerTimeSong == '0NaN:NaN') timeSong.classList.add('imgLoad'), timeSong.innerHTML = ''
 
-        }
 
-        // a vança para proxima musica
-        next() {
-            document.querySelector('.play').classList.add('pause');
-            this.indexSong++
 
-            //verifica se a quantidade de clicks foi igual ao total de musicas na array
-            if (this.indexSong == this.arraySounds.length) this.indexSong = 0;
-            this.renderingSongs(this.indexSong);
-
-        }
-
-        //volta na musica anterior
-        previus() {
-            this.indexSong--
-            document.querySelector('.play').classList.add('pause');
-            if (this.indexSong == -1) this.indexSong = this.arraySounds.length
-            this.renderingSongs(this.indexSong);
-
-        }
-        //ESSA API NATIVA DO JS "PLAYBACKRATE" FAZ COM QUE A VELOCIDADE DA MUSICA MUDE
-        music_speed() {
-            this.audioMp3.playbackRate = 1.5
-        }
     }
+
+    // a vança para proxima musica
+    next() {
+        document.querySelector('.play').classList.add('pause');
+        this.indexSong++
+        //verifica se a quantidade de clicks foi igual ao total de musicas na array
+        if (this.indexSong == this.arraySounds.length) this.indexSong = 0;
+        this.renderingSongs(this.indexSong);
+
+    }
+
+    //volta na musica anterior
+    previus() {
+        this.indexSong--
+        document.querySelector('.play').classList.add('pause');
+        if (this.indexSong == -1) this.indexSong = this.arraySounds.length
+        this.renderingSongs(this.indexSong);
+
+    }
+    //ESSA API NATIVA DO JS "PLAYBACKRATE" FAZ COM QUE A VELOCIDADE DA MUSICA MUDE
+    // music_speed() {
+    //     this.audioMp3.playbackRate = 1.5
+    // }
+}
 
 
 var mp3 = new Mp3();
-mp3.addSongs()
+
 mp3.renderListSongs()
-mp3.indexSongScreen();
+
 
 
